a04701e760d5b969ae275d92080959f9be726bb1
Duke Romkey <dukeromkey@gmail.com>
2022-09-21 21:51:49 -0500

    Initial server setup and basic routing

---
+++ server/Routing/reviewRoute.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+// List Reviews
+router.get('/', (req, res) => {
+  res.send('Reviews Index Hit!');
+  //parameters: page, count, sort, product_id
+});
+
+// Get Review Metadata
+router.get('/meta', (req, res) => {
+  res.send('Metadata Hit');
+  // parameters: product_id
+})
+
+// Add a Review
+router.post('/', (req, res) => {
+  res.send('Reviews index post request hit');
+  // parameters: product_id, rating, summary, body, recommend, name, email, photos, characteristics
+});
+
+// Mark Review as Helpful
+router.put('/:review_id/helpful', (req, res) => {
+  res.send('Helpful review put request hit');
+  //parameter: review_id
+});
+
+// Report Review
+router.put('/:review_id/report', (req, res) => {
+  res.send('Hit report review');
+});
+
+
+
+module.exports = router;
