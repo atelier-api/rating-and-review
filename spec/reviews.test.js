88b88273359410f3961e4057195207db516e8bae
Duke Romkey <dukeromkey@gmail.com>
2022-09-23 20:09:25 -0500

    Added more basic testing for additional routes

--- spec/reviews.test.js
+++ spec/reviews.test.js
@@ -6,9 +6,29 @@ describe("List Reviews", () => {
     await request(app)
       .get("/reviews")
       .expect(200);
-      // .end((err, res) => {
-      //   if (err) return done(err);
-      //   return done();
-      // });
+  });
+
+  test("should get review metadata", async () => {
+    await request(app)
+      .get("/reviews/meta")
+      .expect(200);
+  });
+
+  test("should post a review", async () => {
+    await request(app)
+      .post("/reviews")
+      .expect(201);
+  });
+
+  test("should mark review as helpful", async () => {
+    await request(app)
+      .put("/reviews/:review_id/helpful")
+      .expect(200);
+  });
+
+  test("should report review", async () => {
+    await request(app)
+      .put("/reviews/:review_id/report")
+      .expect(200);
   });
 });
