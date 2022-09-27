06ebefd5e8edcd19ec5bdd04c68b5347c0d3f22e
Duke Romkey <dukeromkey@gmail.com>
2022-09-26 23:50:29 -0500

    Added connection to PostgreSQL

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const app = require("./app");
+const connectDb = require("./db");
 const port = process.env.PORT || 3000;
 
 try {
   app.listen(port, () => {
   console.log(`API running at http://localhost:${port}`);
   });
+  connectDb();
 } catch (err) {
   console.error(err);
   process.exit();
