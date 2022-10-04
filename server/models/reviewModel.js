aaf032522a91418090584bf02f0f42397727c548
Duke Romkey <dukeromkey@gmail.com>
2022-10-03 21:19:03 -0500

    getReviews is returning proper information

--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -1,8 +1,31 @@
 const { pool } = require('../db');
 
 exports.getReviews = async (prodId) => {
-  console.log('start postgres query');
-  const test = await pool.query(`SELECT * FROM reviews WHERE product_id = 72071`);
+  const text = 'SELECT * FROM reviews WHERE product_id = $1';
+  const values = [prodId];
+  console.log('START POSTGRES QUERY');
+  const result = await pool.query(text, values);
+
+  // Shape Data in result.rows
+  const shapedData = await Promise.all(result.rows.map(async (current) => {
+    // query to add photo data
+    const text = 'SELECT id, url FROM photos WHERE review_id = $1';
+    const values = [current.review_id];
+    const photoQuery = await pool.query(text, values);
+    delete current.product_id;
+    delete current.reported;
+    delete current.reviewer_email;
+    delete current.product_id;
+    current.photos = photoQuery.rows;
+    return current;
+  }));
+
   await pool.end();
-  return test;
+
+  return {
+    product: prodId,
+    page: 0,
+    count: 5,
+    results: shapedData
+    }
 };
