1759eac9e9d8c4e8bf636bd4dd08e99a0fde1d11
Duke Romkey <dukeromkey@gmail.com>
2022-10-03 21:19:03 -0500

    getReviews is returning proper information

--- server/db.js
+++ server/db.js
@@ -28,12 +28,12 @@ module.exports = { pool, connectDb };
 
 // Data Table Creation and Import Statements
 
-// CREATE TABLE reviews (id SERIAL, product_id INTEGER, rating INTEGER, date VARCHAR(25), summary TEXT, body TEXT, recommend BOOL, reported BOOL, name VARCHAR(100), email VARCHAR(255), response TEXT, helpfulness INTEGER, PRIMARY KEY(id));
+// CREATE TABLE reviews (review_id SERIAL, product_id INTEGER, rating INTEGER, date VARCHAR(25), summary TEXT, body TEXT, recommend BOOL, reported BOOL, reviewer_name VARCHAR(100), reviewer_email VARCHAR(255), response TEXT, helpfulness INTEGER, PRIMARY KEY(review_id));
 
-// \COPY reviews(id, product_id, rating, date, summary, body, recommend, reported, name, email, response, helpfulness) FROM 'data/reviews.csv' DELIMITER ',' CSV HEADER;
+// \COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM 'data/reviews.csv' DELIMITER ',' CSV HEADER;
 
 
-// CREATE TABLE photos (id SERIAL, review_id INTEGER, url VARCHAR(255), PRIMARY KEY(id), CONSTRAINT fk_review FOREIGN KEY(review_id) REFERENCES reviews(id));
+// CREATE TABLE photos (id SERIAL, review_id INTEGER, url VARCHAR(255), PRIMARY KEY(id), CONSTRAINT fk_review FOREIGN KEY(review_id) REFERENCES reviews(review_id));
 
 // \COPY photos(id, review_id, url) FROM 'data/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 
@@ -43,6 +43,6 @@ module.exports = { pool, connectDb };
 // \COPY characteristics(id, product_id, name) FROM 'data/characteristics.csv' DELIMITER ',' CSV HEADER;
 
 
-// CREATE TABLE characteristic_reviews (id SERIAL, characteristic_id INTEGER, review_id INTEGER, value INTEGER, PRIMARY KEY(id), CONSTRAINT fk_characteristic_review FOREIGN KEY(review_id) REFERENCES reviews(id), CONSTRAINT fk_characteristics FOREIGN KEY(characteristic_id) REFERENCES characteristics(id));
+// CREATE TABLE characteristic_reviews (id SERIAL, characteristic_id INTEGER, review_id INTEGER, value INTEGER, PRIMARY KEY(id), CONSTRAINT fk_characteristic_review FOREIGN KEY(review_id) REFERENCES reviews(review_id), CONSTRAINT fk_characteristics FOREIGN KEY (characteristic_id) REFERENCES characteristics(id));
 
 // \COPY characteristic_reviews(id, characteristic_id, review_id, value) FROM 'data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
