f511169a9ee3b6469a94212a99e969fe38b6a5c6
Duke Romkey <dukeromkey@gmail.com>
2022-10-29 15:48:32 -0500

    t status

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ export const options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 2000,
+      rate: 100,
       timeunit: '1s',
       duration: '1m',
-      preAllocatedVUs: 200,
-      maxVUs: 200,
+      preAllocatedVUs: 10,
+      maxVUs: 10,
     }
   }
 }
 
 export default function() {
   const rnd = Math.floor(Math.random() * (1000011 - 900010 + 1) + 900010);
-  const res = http.get(`http://localhost:2000/reviews/meta/${rnd}`);
+  const res = http.get(`http://localhost:2000/reviews/${rnd}`);
   check(res, {
     'is status 200': (r) => r.status === 200,
   });
