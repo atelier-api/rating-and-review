48c2040b4324a3dfc069c77e1e7e67a634ca20e7
Duke Romkey <dukeromkey@gmail.com>
2022-11-04 21:33:46 -0500

    new loader file

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 app.use('/reviews', reviewRoute);
 
-app.get('/loaderio-eda1c42c66a346ef4c9d417ddd5cc475.txt', (req, res) => {
-  res.sendFile(`${__dirname}/loaderio-eda1c42c66a346ef4c9d417ddd5cc475.txt`);
+app.get('/loaderio-398d1e422ef47a2e3e30fe05f51d308a.txt', (req, res) => {
+  res.sendFile(`${__dirname}/loaderio-398d1e422ef47a2e3e30fe05f51d308a.txt`);
 });
 
 module.exports = app;
